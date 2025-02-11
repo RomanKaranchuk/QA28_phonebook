c251b1948dab7f5a1f7aab0196a104207c436008
RomanKaranchuk <picasa.otvechauu@yandex.ru>
2025-02-11 23:08:14 +0200

    additions
    additions

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,17 +1,18 @@
 package manager;
 
+import models.User;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 
-public class HelperUser extends HelperBase{
+public class HelperUser extends HelperBase {
 
     public HelperUser(WebDriver wd) {
         super(wd);
     }
 
-    public void openLoginRegistrationForm(){
+    public void openLoginRegistrationForm() {
 //    WebElement loginTab = wd.findElement(By.cssSelector("a[href='/login']"));
 //    //xPath--> //a[text()='LOGIN']
 //    loginTab.click();
@@ -21,31 +22,25 @@ public class HelperUser extends HelperBase{
 
 
     public void fillLoginRegistrationForm(String email, String password) {
-
-//        WebElement emailInput = wd.findElement(By.name("email"));
-//        emailInput.click();
-//        emailInput.clear();
-//        emailInput.sendKeys(email);
-        type(By.name("email"),email);
-
-
-//        WebElement passwordInput = wd.findElement(By.xpath("//input[last()]"));
-//        passwordInput.click();
-//        passwordInput.clear();
-//        passwordInput.sendKeys(password);
-        type(By.xpath("//input[last()]"),password);
+        type(By.name("email"), email);
+        type(By.xpath("//input[last()]"), password);
     }
 
+    public void fillLoginRegistrationForm(User user) {
+        type(By.name("email"), user.getEmail());
+        type(By.xpath("password"), user.getPassword());
+    }
 
-    public void sumitLogin(){
+    public void submitLogin() {
         click(By.xpath("//button[text()='Login']"));
     }
 
     public boolean isLogged() {
-        return   isElementPresent(By.xpath("//button[text() = 'Sign Out']"));
+        return isElementPresent(By.xpath("//button[text() = 'Sign Out']"));
     }
 
     public void logout() {
         click(By.xpath("//button[text() = 'Sign Out']"));
     }
+
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,4 +1,6 @@
 package tests;
+
+import models.User;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -6,9 +8,9 @@ import org.testng.annotations.Test;
 public class LoginTests extends TestBase {
 
     @BeforeMethod
-    public void preCondition(){
+    public void preCondition() {
         //if SignOut present --->logout
-        if(app.getHelperUser().isLogged())
+        if (app.getHelperUser().isLogged())
             app.getHelperUser().logout();
     }
 
@@ -16,8 +18,8 @@ public class LoginTests extends TestBase {
     @Test
     public void loginSuccess() {
         app.getHelperUser().openLoginRegistrationForm();
-        app.getHelperUser().fillLoginRegistrationForm("mara@gmail.com","Mmar123456$");
-        app.getHelperUser().sumitLogin();
+        app.getHelperUser().fillLoginRegistrationForm("mara@gmail.com", "Mmar123456$");
+        app.getHelperUser().submitLogin();
         //Assert
 //        Assert.assertEquals();
 //        Assert.assertNotEquals();
@@ -27,11 +29,22 @@ public class LoginTests extends TestBase {
     }
 
     @Test
-    public void loginSuccessModel() {
+    public void loginSuccess1() {
+        User user = new User().setEmail("romaabcd@maile.com").setPassword("Abcd12345$");
+
         app.getHelperUser().openLoginRegistrationForm();
-        app.getHelperUser().fillLoginRegistrationForm("mara@gmail.com","Mmar123456$");
-        app.getHelperUser().sumitLogin();
+        app.getHelperUser().fillLoginRegistrationForm(user);
+        app.getHelperUser().submitLogin();
         Assert.assertTrue(app.getHelperUser().isLogged());
     }
+
+    @Test
+    public void loginSuccessModel() {
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("mara@gmail.com", "Mmar123456$");
+        app.getHelperUser().submitLogin();
+        Assert.assertTrue(app.getHelperUser().isLogged());
+    }
+
 
 }
